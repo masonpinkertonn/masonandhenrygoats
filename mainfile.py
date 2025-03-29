6d211f1713b92f89b294b9a75a3aa8aa34bc8507
masonpinkertonn <mason.pinkerton28@paceacademy.org>
2025-03-29 00:08:48 -0400

    Update mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -1,1 +1,11 @@
 import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
