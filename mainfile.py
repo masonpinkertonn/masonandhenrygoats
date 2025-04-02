d21b49b96dd827817438614552bf9f2c1d39610f
masonpinkertonn <mason.pinkerton28@paceacademy.org>
2025-04-02 14:05:56 -0400

    stuff

--- mainfile.py
+++ mainfile.py
@@ -5,12 +5,40 @@ import random
 import math
 import time
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.sprites = []
+        self.sprites.append(pygame.image.load('tile000.png'))
+        self.sprites.append(pygame.image.load('tile001.png'))
+        self.sprites.append(pygame.image.load('tile002.png'))
+        self.sprites.append(pygame.image.load('tile003.png'))
+        self.sprites.append(pygame.image.load('tile004.png'))
+        self.sprites.append(pygame.image.load('tile005.png'))
+        self.currentsprite = 0
+        self.image = self.sprites[self.currentsprite]
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [x, y]
+    def update(self):
+        self.currentsprite += 0.2
+
+        if self.currentsprite >= len(self.sprites):
+            self.currentsprite = 0
+
+        self.image = self.sprites[int(self.currentsprite)]
+
+
 pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+x = 0
+y = 255
+
+"""
 player = pygame.image.load('Biker_idle.png')
 width = player.get_rect().width
 width *= 2
@@ -22,10 +50,9 @@ jumpwidth = playerjump.get_rect().width
 jumpwidth *= 2
 jumpheight = playerjump.get_rect().height
 jumpheight *= 2
+"""
 
 running = True
-x = 0
-y = 255
 clock = pygame.time.Clock()
 
 isjumping = False
@@ -34,25 +61,30 @@ y_gravity = 1
 jump_height = 20
 y_vel = jump_height
 
-delta_time = 0.1
+movingsprites = pygame.sprite.Group()
+player = Player(10,10)
+movingsprites.add(player)
 
 while running:
 
     screen.fill((0,0,0))
 
+    """
+
     player = pygame.transform.scale(player, (width, height))
     playerjump = pygame.transform.scale(playerjump, (jumpwidth, jumpheight))
-
     screen.blit(player, (x, y))
 
     key = pygame.key.get_pressed()
-
+    """
+    movingsprites.draw(screen)
+    movingsprites.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     keys = pygame.key.get_pressed()
-
+    """
     if keys[pygame.K_LEFT]:
         screen.fill((0,0,0))
         screen.blit(pygame.transform.flip(player, True, False), (x, y))
@@ -70,10 +102,9 @@ while running:
         if y_vel < -jump_height:
             isjumping = False
             y_vel = jump_height
+    """
 
     pygame.display.flip()
-
-    delta_time = clock.tick(60) / 1000
-    delta_time = max(0.001, min(0.1, delta_time))
+    clock.tick(60)
 
 pygame.quit()
