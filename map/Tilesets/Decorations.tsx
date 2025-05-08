f84c3764c3600f1ec86eda8e34fa0efcf9f0acc2
Kovarian79 <henry.coburn28@paceacademy.org>
2025-05-08 11:48:22 -0400

    TMX working

---
+++ map/Tilesets/Decorations.tsx
@@ -0,0 +1,169 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Decorations" tilewidth="320" tileheight="256" tilecount="55" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../Tiny Swords (Update 010)/Deco/01.png" width="64" height="64"/>
+ </tile>
+ <tile id="1">
+  <image source="../Tiny Swords (Update 010)/Deco/02.png" width="64" height="64"/>
+ </tile>
+ <tile id="2">
+  <image source="../Tiny Swords (Update 010)/Deco/03.png" width="64" height="64"/>
+ </tile>
+ <tile id="3">
+  <image source="../Tiny Swords (Update 010)/Deco/04.png" width="64" height="64"/>
+ </tile>
+ <tile id="4">
+  <image source="../Tiny Swords (Update 010)/Deco/05.png" width="64" height="64"/>
+ </tile>
+ <tile id="5">
+  <image source="../Tiny Swords (Update 010)/Deco/06.png" width="64" height="64"/>
+ </tile>
+ <tile id="6">
+  <image source="../Tiny Swords (Update 010)/Deco/07.png" width="64" height="64"/>
+ </tile>
+ <tile id="7">
+  <image source="../Tiny Swords (Update 010)/Deco/08.png" width="64" height="64"/>
+ </tile>
+ <tile id="8">
+  <image source="../Tiny Swords (Update 010)/Deco/09.png" width="64" height="64"/>
+ </tile>
+ <tile id="9">
+  <image source="../Tiny Swords (Update 010)/Deco/10.png" width="64" height="64"/>
+ </tile>
+ <tile id="10">
+  <image source="../Tiny Swords (Update 010)/Deco/11.png" width="64" height="64"/>
+ </tile>
+ <tile id="11">
+  <image source="../Tiny Swords (Update 010)/Deco/12.png" width="64" height="64"/>
+ </tile>
+ <tile id="12">
+  <image source="../Tiny Swords (Update 010)/Deco/13.png" width="64" height="64"/>
+ </tile>
+ <tile id="13">
+  <image source="../Tiny Swords (Update 010)/Deco/14.png" width="64" height="64"/>
+ </tile>
+ <tile id="14">
+  <image source="../Tiny Swords (Update 010)/Deco/15.png" width="64" height="64"/>
+ </tile>
+ <tile id="15">
+  <image source="../Tiny Swords (Update 010)/Deco/16.png" width="64" height="128"/>
+ </tile>
+ <tile id="16">
+  <image source="../Tiny Swords (Update 010)/Deco/17.png" width="64" height="128"/>
+ </tile>
+ <tile id="17">
+  <image source="../Tiny Swords (Update 010)/Deco/18.png" width="192" height="192"/>
+ </tile>
+ <tile id="18">
+  <image source="../Tiny Swords (Update 010)/Resources/Gold Mine/GoldMine_Active.png" width="192" height="128"/>
+ </tile>
+ <tile id="19">
+  <image source="../Tiny Swords (Update 010)/Resources/Gold Mine/GoldMine_Destroyed.png" width="192" height="128"/>
+ </tile>
+ <tile id="20">
+  <image source="../Tiny Swords (Update 010)/Resources/Gold Mine/GoldMine_Inactive.png" width="192" height="128"/>
+ </tile>
+ <tile id="21">
+  <image source="../Tiny Swords (Update 010)/Resources/Resources/G_Idle_(NoShadow).png" width="128" height="128"/>
+ </tile>
+ <tile id="22">
+  <image source="../Tiny Swords (Update 010)/Resources/Resources/G_Idle.png" width="128" height="128"/>
+ </tile>
+ <tile id="23">
+  <image source="../Tiny Swords (Update 010)/Resources/Resources/M_Idle_(NoShadow).png" width="128" height="128"/>
+ </tile>
+ <tile id="24">
+  <image source="../Tiny Swords (Update 010)/Resources/Resources/M_Idle.png" width="128" height="128"/>
+ </tile>
+ <tile id="25">
+  <image source="../Tiny Swords (Update 010)/Resources/Trees/Tree copy 4.png" width="193" height="111"/>
+ </tile>
+ <tile id="26">
+  <image source="../Tiny Swords (Update 010)/Resources/Trees/Tree copy 2.png" width="142" height="210"/>
+ </tile>
+ <tile id="27">
+  <image source="../Tiny Swords (Update 010)/Resources/Trees/Tree copy 3.png" width="118" height="190"/>
+ </tile>
+ <tile id="28">
+  <image source="../Tiny Swords (Update 010)/Resources/Trees/Tree copy.png" width="139" height="190"/>
+ </tile>
+ <tile id="29">
+  <image source="../Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Destroyed.png" width="256" height="192"/>
+ </tile>
+ <tile id="30">
+  <image source="../Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_InConstruction.png" width="256" height="192"/>
+ </tile>
+ <tile id="31">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/House/House_Blue.png" width="128" height="192"/>
+ </tile>
+ <tile id="32">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/House/House_Construction.png" width="128" height="192"/>
+ </tile>
+ <tile id="33">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/House/House_Destroyed.png" width="128" height="192"/>
+ </tile>
+ <tile id="34">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/House/House_Purple.png" width="128" height="192"/>
+ </tile>
+ <tile id="35">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/House/House_Red.png" width="128" height="192"/>
+ </tile>
+ <tile id="36">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/House/House_Yellow.png" width="128" height="192"/>
+ </tile>
+ <tile id="37">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Tower/Tower_Blue.png" width="128" height="256"/>
+ </tile>
+ <tile id="38">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Tower/Tower_Construction.png" width="128" height="256"/>
+ </tile>
+ <tile id="39">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Tower/Tower_Destroyed.png" width="128" height="256"/>
+ </tile>
+ <tile id="40">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Tower/Tower_Purple.png" width="128" height="256"/>
+ </tile>
+ <tile id="41">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Tower/Tower_Red.png" width="128" height="256"/>
+ </tile>
+ <tile id="42">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Tower/Tower_Yellow.png" width="128" height="256"/>
+ </tile>
+ <tile id="43">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Castle/Castle_Blue.png" width="320" height="256"/>
+ </tile>
+ <tile id="44">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Castle/Castle_Construction.png" width="320" height="256"/>
+ </tile>
+ <tile id="45">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Castle/Castle_Destroyed.png" width="320" height="256"/>
+ </tile>
+ <tile id="46">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Castle/Castle_Purple.png" width="320" height="256"/>
+ </tile>
+ <tile id="47">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Castle/Castle_Red.png" width="320" height="256"/>
+ </tile>
+ <tile id="48">
+  <image source="../Tiny Swords (Update 010)/Factions/Knights/Buildings/Castle/Castle_Yellow.png" width="320" height="256"/>
+ </tile>
+ <tile id="49">
+  <image source="../Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_House/Goblin_House_Destroyed.png" width="128" height="192"/>
+ </tile>
+ <tile id="50">
+  <image source="../Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_House/Goblin_House.png" width="128" height="192"/>
+ </tile>
+ <tile id="51">
+  <image source="../Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Red.aseprite" width="256" height="192"/>
+ </tile>
+ <tile id="52">
+  <image source="../Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Blue.aseprite" width="256" height="192"/>
+ </tile>
+ <tile id="53">
+  <image source="../Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Purple.aseprite" width="256" height="192"/>
+ </tile>
+ <tile id="54">
+  <image source="../Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Yellow.aseprite" width="256" height="192"/>
+ </tile>
+</tileset>
